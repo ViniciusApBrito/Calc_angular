fc909a1932302724b192a44fe6a7b9914e452e95
beamatos03 <bea.matos1978@gmail.com>
2023-04-04 14:56:54 -0300

    calc-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
+import { formatCurrency } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalcComponent } from './calc/calc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalcComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
